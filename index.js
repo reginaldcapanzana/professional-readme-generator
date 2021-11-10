866502cf660d0bc7631e447af9a72f99ff17af0b
Reginald Capanzana <reggiecapanzana@gmail.com>
2021-11-09 21:53:04 -0800

    Updated questions array, updated init function with inquirer prompt

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const questions = [
     "Provide project installation steps",
     "Provide usage of project",
     "Provide credits",
-    "Provide license",
+    "Provide license", //List of options
     "Provide features",
     "Provide how to contribute",
-    "Provide tests"
+    "Provide tests",
+    "Enter github username", //last two used for Questions section
+    "Enter email address"
 ];
 
 // TODO: Create a function to write README file
@@ -28,6 +30,74 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+//Call inquirer to prompt user for input
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "title",
+                message: questions[0],
+            },
+            {
+                type: "input",
+                name: "description",
+                message: questions[1],
+            },
+            {
+                type: "input",
+                name: "installation",
+                message: questions[2],
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: questions[3],
+            },
+            {
+                type: "input",
+                name: "credits",
+                message: questions[4],
+            },
+            {
+                type: "list",
+                name: "license",
+                message: questions[5],
+                choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-2-Clause', 'BSD-3-Clause', 'BSD-4-Clause', 'None'],
+            },
+            {
+                type: "input",
+                name: "features",
+                message: questions[6],
+            },
+            {
+                type: "input",
+                name: "contribution",
+                message: questions[7],
+            },
+            {
+                type: "input",
+                name: "tests",
+                message: questions[8],
+            },
+            {
+                type: "input",
+                name: "username",
+                message: questions[9],
+            },
+            {
+                type: "input",
+                name: "email",
+                message: questions[10],
+            },
+        ])
+        .then((data) => {
+            console.log(data);
+            // const{title} = data;
+            // const{description} = data;
+
+        });
+//Use generateMarkdown js file to create markdown
+//Call writeToFile to create readme file
 
 }
 
