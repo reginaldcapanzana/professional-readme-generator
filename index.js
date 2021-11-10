29e30dadfba552acf184544b2b3e51d92eaf79fa
Reginald Capanzana <reggiecapanzana@gmail.com>
2021-11-09 20:18:34 -0800

    Added questions to array

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Provide project title",
+    "Provide a short description of your project",
+    "Provide project installation steps",
+    "Provide usage of project",
+    "Provide credits",
+    "Provide license",
+    "Provide features",
+    "Provide how to contribute",
+    "Provide tests"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
