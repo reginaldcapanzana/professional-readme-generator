1dfbe21879458053a010b65b161a7641b6fcd52b
Reginald Capanzana <reggiecapanzana@gmail.com>
2021-11-09 20:28:00 -0800

    Included generateMarkdown package and implemented writeToFile function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // TODO: Include packages needed for this application
+// DONE
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+// DONE
 const questions = [
     "Provide project title",
     "Provide a short description of your project",
@@ -16,10 +19,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// DONE
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log("Success!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
